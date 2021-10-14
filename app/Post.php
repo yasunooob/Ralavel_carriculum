d156a2dea99d031ccf6b2583f4385699f30d8ae7
yasunooob <yasunobu818@gmail.com>
2021-10-15 02:35:19 +0900

    [add]記事削除

--- app/Post.php
+++ app/Post.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
@@ -11,4 +12,5 @@ class Post extends Model
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
     protected $fillable = ['title', 'body'];
+    use SoftDeletes;
 }
